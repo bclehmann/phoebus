c991d6e990108ac5eecf6964855f748e21368906
Benjamin Lehmann <bclehmann@yahoo.ca>
2021-07-25 18:30:05 -0600

    Creates some tests

---
+++ tests/__tests__/performance.js
@@ -0,0 +1,34 @@
+import { jest, describe, it, expect } from "@jest/globals";
+import { Store } from "../../src/common/dist";
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+describe("avoids unnecessary refetches", () => {
+  let counter = 0;
+  const store = new Store();
+  const query = store.createQuery(
+    "foo",
+    async () => {
+      await sleep(500);
+      return counter++;
+    },
+    {}
+  );
+
+  it("doesn't invoke if a promise is in flight", () => {
+    query.getResult().then((r) => expect(r).toEqual(0));
+    query.getResult().then((r) => expect(r).toEqual(0));
+    query.getResult().then((r) => expect(r).toEqual(0));
+    query.getResult().then((r) => expect(r).toEqual(0));
+    query.getResult().then((r) => expect(r).toEqual(0));
+    query.getResult().then((r) => expect(r).toEqual(0));
+  });
+
+  it("respects forceRefresh", () => {
+    query.getResult(true).then((r) => expect(r).toEqual(1));
+    query.getResult(true).then((r) => expect(r).toEqual(2));
+    query.getResult(true).then((r) => expect(r).toEqual(3));
+  });
+});
+
+export {};
