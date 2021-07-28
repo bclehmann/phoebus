96dbe4ec5b9d46e263f6a5c6ffdd2fc65fa3bbf1
Benjamin Lehmann <bclehmann@yahoo.ca>
2021-07-27 19:10:22 -0600

    Adds tests for correctness on new StoreEntry model

--- tests/__tests__/correctness.js
+++ tests/__tests__/correctness.js
@@ -5,10 +5,18 @@ describe("produces correct output", () => {
   let count = 0;
   const identityFunction = (x) => x;
   const countingFunction = () => count++;
-  
+
   const store = new Store();
-  const identityQuery = store.createQuery("identityQuery", identityFunction, "A");
-  const countingQuery = store.createQuery("countingQuery", countingFunction, {});
+  const identityQuery = store.createQuery(
+    "identityQuery",
+    identityFunction,
+    "A"
+  );
+  const countingQuery = store.createQuery(
+    "countingQuery",
+    countingFunction,
+    {}
+  );
 
   it("refetches on set", async () => {
     const res1 = await identityQuery.getResult();
@@ -35,16 +43,40 @@ describe("produces correct output", () => {
     const res2 = await countingQuery.getResult(true);
     await expect(res2).toEqual(1);
   });
+
+  it("getValue returns null if the StoreEntry does not exist", () => {
+    return expect(store.getValue("invalid")).toBeNull();
+  });
+
+  it("getValue and getCurrentValue return null if the Query has never ran", () => {
+    const testQuery = store.createQuery("test", () => "A", "A");
+
+    return Promise.all([
+      expect(store.getValue("test", "A")).toBeNull(),
+      expect(store.getCurrentValue("test")).toBeNull(),
+    ]);
+  });
+
+  it("correctly fetches old data", async () => {
+    identityQuery.body = "1";
+    expect(await identityQuery.getResult()).toBe("1");
+
+    identityQuery.body = "2";
+    expect(await identityQuery.getResult()).toBe("2");
+
+    identityQuery.body = "1";
+    expect(await identityQuery.getResult()).toBe("1");
+  })
 });
 
 describe("preserves invariants", () => {
-    const identityFunction = (x) => x;
-    const store = new Store();
+  const identityFunction = (x) => x;
+  const store = new Store();
 
-    it("ensures storageKeys are always unique within a store", () => {
-        return expect(() => {
-            store.createQuery("key", identityFunction, {});
-            store.createQuery("key", identityFunction, {});
-        }).toThrow();
-    });
-})
+  it("ensures storageKeys are always unique within a store", () => {
+    return expect(() => {
+      store.createQuery("key", identityFunction, {});
+      store.createQuery("key", identityFunction, {});
+    }).toThrow();
+  });
+});
