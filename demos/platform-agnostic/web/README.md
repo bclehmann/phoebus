0b73a9cff9e57b3eda6b85d5a8419b0adebec38a
Benjamin Lehmann <bclehmann@yahoo.ca>
2021-08-08 18:38:47 -0600

    Update demos

--- demos/platform-agnostic/web/README.md
+++ demos/platform-agnostic/web/README.md
@@ -1,3 +1,1 @@
-Phoebus doesn't *need* to be used for web APIs, it's general enough to be used as a smart cache on any operation you want.
-
 This demo is tested with [ts-node](https://github.com/TypeStrong/ts-node) for convenience, although you could absolutely set up your own toolchain if you so desired.
---
+++ demos/platform-agnostic/web/index.ts
@@ -0,0 +1,40 @@
+import { WebQuery } from "../../../src/web";
+
+interface Response {
+  data: User;
+  support: Support;
+}
+
+interface Support {
+  url: string;
+  text: string;
+}
+
+interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+const apiDelay = 1;
+
+// See https://reqres.in/ for a mock API
+const getUserUri = (id: number) =>
+  `https://reqres.in/api/users/${id}?delay=${apiDelay}`;
+const query = new WebQuery<{}, Response>(getUserUri(1), "GET");
+
+let i = 1;
+
+setInterval(async () => {
+  const start = Date.now();
+  const res = await query.getResult();
+  console.log(`ID: ${i}`);
+  console.log(`Email: ${res!.body.data.email}`);
+  console.log(`Time Taken: ${Date.now() - start}ms\n`);
+
+  i %= 3;
+  i++;
+  query.uri = getUserUri(i);
+}, 2000);
